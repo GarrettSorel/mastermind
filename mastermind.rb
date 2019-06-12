53dc125049232ecc4b974635146e818a2617fddc
GarrettSorel <garrettsorel@gmail.com>
2019-06-12 13:14:25 -0400

    general method and class setup

--- mastermind.rb
+++ mastermind.rb
@@ -1,13 +1,51 @@
 class Game
 
+  @@cells = Hash.new
+  @@pegs = {
+    "black" => "BK",
+    "green" => "GR",
+    "red" => "RD",
+    "blue" => "BL",
+    "yellow" => "YW"
+  }
+
   def initialize
+    @board = GameBoard.new
+    welcome
+  end
+
+  def welcome
+    puts "****Welcome to Mastermind****"
+    puts "****Do You Have What It****"
+    puts "****Takes To Crack the Code?****"
+    @board.update
+  end
+
+  def turn
+  end
+
+  def select
+  end
+
+  def clue_pegs
+  end
+
+  def victory?
+  end
+
+  private
+
+  def generate_code
   end
 
 end
 
-class GameBoard
+class GameBoard < Game
 
   def initialize
   end
-  
+
+  def update
+  end
+
 end
